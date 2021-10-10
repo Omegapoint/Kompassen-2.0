a5cfc74748a99c098eb77693e23b75fd432b9827
Markus Örebrand <markus.orebrand@gmail.com>
2021-10-10 21:20:47 +0200

    [bugfix] Replaced hardcoded example name with Azure details

--- webapp/src/section/settings/Profile.tsx
+++ webapp/src/section/settings/Profile.tsx
@@ -1,5 +1,6 @@
 import { createStyles, makeStyles, Typography } from '@material-ui/core';
 import { ReactElement } from 'react';
+import { useAppSelector } from '../../lib/Lib';
 import { padding } from '../../theme/Theme';
 
 const useStyles = makeStyles(() =>
@@ -19,13 +20,14 @@ const useStyles = makeStyles(() =>
 
 const Profile = (): ReactElement => {
   const classes = useStyles();
+  const { azureUser } = useAppSelector((state) => state.session);
 
   return (
     <div className={classes.myProfile}>
       <Typography>Namn</Typography>
-      <Typography className={classes.bold}>Susane Bakoush</Typography>
+      <Typography className={classes.bold}>{azureUser.displayName}</Typography>
       <Typography>Epost</Typography>
-      <Typography className={classes.bold}>susan.bakoush@omegapoint</Typography>
+      <Typography className={classes.bold}>{azureUser.mail}</Typography>
     </div>
   );
 };
