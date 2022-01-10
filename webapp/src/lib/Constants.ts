5d3a164b91f8e7e9ace1dac92bc5ac7dcb7a3bac
Victoria Catalán <victoria.catalan@omegapoint.se>
2022-01-10 15:27:53 +0100

    [fix] extend character limits

--- webapp/src/lib/Constants.ts
+++ webapp/src/lib/Constants.ts
@@ -1,4 +1,4 @@
 export const STRING_MIN_LEN = 1;
-export const SHORT_STRING_LEN = 50;
-export const LARGE_STRING_LEN = 500;
+export const SHORT_STRING_LEN = 100;
+export const LARGE_STRING_LEN = 1000;
 export const FILE_LEN = 1024 * 1024;
