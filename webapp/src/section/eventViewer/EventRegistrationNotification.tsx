00891a19b14de8553ca9875cea5f26e089f6de42
Victoria Catalán <victoria.catalan@omegapoint.se>
2022-01-12 12:27:57 +0100

    fix arrow-body-style

--- webapp/src/section/eventViewer/EventRegistrationNotification.tsx
+++ webapp/src/section/eventViewer/EventRegistrationNotification.tsx
@@ -7,31 +7,30 @@ interface EventRegistrationNoteProps {
   message?: string;
 }
 
-const EventRegistrationNote = ({ title, message }: EventRegistrationNoteProps): ReactElement => {
-  return (
-    <Box sx={{ display: 'grid', justifyItems: 'center' }}>
-      <Paper
-        sx={{
-          display: 'grid',
-          justifyContent: 'center',
-          gridGap: padding.minimal,
-          padding: padding.medium,
-          '& h6': {
-            lineHeight: 1,
-          },
-        }}
-      >
-        <Typography variant="h4" align="center">
-          {title}
+const EventRegistrationNote = ({title, message}: EventRegistrationNoteProps): ReactElement => (
+  <Box sx={{display: 'grid', justifyItems: 'center'}}>
+    <Paper
+      sx={{
+        display: 'grid',
+        justifyContent: 'center',
+        gridGap: padding.minimal,
+        padding: padding.medium,
+        '& h6': {
+          lineHeight: 1,
+        },
+      }}
+    >
+      <Typography variant="h4" align="center">
+        {title}
+      </Typography>
+      {!!message && (
+        <Typography variant="body1" align="center">
+          {message}
         </Typography>
-        {!!message && (
-          <Typography variant="body1" align="center">
-            {message}
-          </Typography>
-        )}
-      </Paper>
-    </Box>
-  );
-};
+      )}
+    </Paper>
+  </Box>
+
+);
 
 export default EventRegistrationNote;
