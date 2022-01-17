c59ebbe10c1441d4d2569e69fcfa84682d537b6a
Dennis Granberg <dennis.granberg@omegapoint.se>
2022-01-17 15:21:26 +0100

    Removed duplicate code after merge

--- webapp/src/section/eventViewer/EventRegistration.tsx
+++ webapp/src/section/eventViewer/EventRegistration.tsx
@@ -20,7 +20,6 @@ import SmallLoader from '../../components/loader/SmallLoader';
 import useForm from '../../hooks/UseForm';
 import { useAppSelector } from '../../lib/Lib';
 import { Event, Lecture } from '../../lib/Types';
-import Schedule from '../eventPlanner/Schedule';
 import SmallLectureCard from '../eventPlanner/SmallLectureCard';
 
 const defaultFormValue: FormValue = {
@@ -130,7 +129,6 @@ const EventRegistration = ({
   }
   return (
     <Box sx={{ display: 'grid', gridGap: '10px', marginTop: '40px' }}>
-      <Schedule lectures={approvedLectures} event={event} editable={false} />
       <Typography variant="h4">Anmäl dig till kompetensdagen</Typography>
       <FormControlLabel
         control={
