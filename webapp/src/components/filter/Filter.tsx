1830e3d3ae3272a5f1898db01c14a299e48a5007
Christoffer Blomberg <98954229+OmegaChristoffer@users.noreply.github.com>
2022-02-23 10:59:35 +0100

    Removed as string, missed in previous.

--- webapp/src/components/filter/Filter.tsx
+++ webapp/src/components/filter/Filter.tsx
@@ -36,8 +36,8 @@ const handleSort = (value: string, lectures: Lecture[]): Lecture[] => {
 };
 
 // Show cards that is scheduled for the future.
-const findFutureEvent = (events: Event[]): string => 
-  return events.find(e => e.startAt > new Date())?.id as string;
+const findFutureEvent = (events: Event[]) => 
+  return events.find(e => e.startAt > new Date())?.id;
 
 // filter the "Filtrera" - dropdown
 const handleFilters = (value: string, sorted: Lecture[], events: Event[]): Lecture[] => {
