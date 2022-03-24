439834d39a511b532de4f8d00410e647bb5964bf
Hannes-Samskog <90246748+Hannes-Samskog@users.noreply.github.com>
2022-03-24 08:55:27 +0100

    Op ko ko form (#255)
    * first draft OpKoKo-form
    
    * Slightly improved text and padding
    
    * added question
    
    Co-authored-by: Mirjam Borén <mirjamboren@Mirjams-MacBook-Pro.local>

--- webapp/src/section/lecture/Lecture.tsx
+++ webapp/src/section/lecture/Lecture.tsx
@@ -3,11 +3,13 @@ import { ReactElement } from 'react';
 import { getLecture } from '../../api/Api';
 import FormWrapper from '../../components/formWrapper/FormWrapper';
 import LectureForm from '../../components/lectureForm/LectureForm';
+import OpKoKoForm from '../../components/opKoKoForm/OpKoKoForm';
 
 const Lecture = (): ReactElement => (
   <Box sx={{ width: '50vw' }}>
     <FormWrapper fn={getLecture} name="lecture">
-      <LectureForm />
+      <OpKoKoForm />
+      {/* <LectureForm /> */}
     </FormWrapper>
   </Box>
 );
