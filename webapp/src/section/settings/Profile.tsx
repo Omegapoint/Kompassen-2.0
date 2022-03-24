f45b02ca2d4526257d60bf3fb47fc87c74f772a0
Hannes Samskog <hannes.samskog@omegapoint.se>
2022-03-24 20:14:05 +0100

    First draft for fetching AD images

--- webapp/src/section/settings/Profile.tsx
+++ webapp/src/section/settings/Profile.tsx
@@ -14,9 +14,37 @@ import { updateUser } from '../../api/Api';
 import useInvalidateOnSuccess from '../../hooks/UseInvalidateOnSuccess';
 import { useAppSelector } from '../../lib/Lib';
 import { Office } from '../../lib/Types';
+import store from '../../reducers';
 import { padding } from '../../theme/Theme';
 
+export async function getAzureGraphImage(): Promise<string> {
+  const accessToken = store.getState().session.graphToken;
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
+  const result = await fetch(
+    `https://graph.microsoft.com/v1.0/users/33ff501d-fd05-4e18-a318-12ad6608ef0b/photo/$value`,
+    options
+  )
+    // eslint-disable-next-line no-console
+    .catch((error) => console.log(error));
+  if (result instanceof Object && result.body !== null) {
+    const reader = result.body.getReader();
+    const test2 = await reader.read();
+    if (test2.value !== undefined) {
+      return URL.createObjectURL(new Blob([test2.value], { type: 'image/png' } /* (1) */));
+    }
+    throw new Error();
+  }
+  throw new Error();
+}
+
 const Profile = (): ReactElement => {
+  const [test, setTest] = useState('');
   const offices = useAppSelector((state) => state.offices);
   const { azureUser } = useAppSelector((state) => state.session);
   const user = useAppSelector((state) => state.user);
@@ -24,6 +52,36 @@ const Profile = (): ReactElement => {
   useInvalidateOnSuccess(isSuccess, 'user');
 
   const [selected, setSelected] = useState(user.officeId);
+
+  useEffect(() => {
+    const getPhoto = async () => {
+      const accessToken = store.getState().session.graphToken;
+      const options = {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      const result = await fetch(
+        `https://graph.microsoft.com/v1.0/users/33ff501d-fd05-4e18-a318-12ad6608ef0b/photo/$value`,
+        options
+      )
+        // eslint-disable-next-line no-console
+        .catch((error) => console.log(error));
+      if (result instanceof Object && result.body !== null) {
+        const reader = result.body.getReader();
+        const test2 = await reader.read();
+        if (test2.value !== undefined) {
+          const test3 = new Blob([test2.value], { type: 'image/png' } /* (1) */);
+
+          const url = window.URL || window.webkitURL;
+          const blobUrl = url.createObjectURL(test3);
+          setTest(blobUrl);
+        }
+      }
+    };
+    getPhoto();
+  }, []);
 
   useEffect(() => {
     mutate({
@@ -37,35 +95,48 @@ const Profile = (): ReactElement => {
   const handleChange = (event: SelectChangeEvent<Office>) => {};
 
   return (
-    <><Box
-      sx={{
-        display: 'grid',
-        gridGap: `${padding.minimal} ${padding.large}`,
-        paddingTop: padding.standard,
-        paddingBottom: padding.large,
-        gridTemplateColumns: 'max-content max-content',
-      }}
-    >
-      <Typography>Namn</Typography>
-      <Typography sx={{ fontWeight: 400 }}>{azureUser.displayName}</Typography>
-      <Typography>Epost</Typography>
-      <Typography sx={{ fontWeight: 400 }}>{azureUser.mail}</Typography></Box>
-    <Box marginTop={0}>
-      <TextField fullWidth multiline minRows={7} required name="Speaker bio" label="Talarbio: (visas upp på dina kompetensbidrag, var tydlig med din roll och din kompetens!)" variant="outlined" />
-      </Box>
-    <Box marginTop={0} marginBottom={5}><FormControl fullWidth>
-      <InputLabel id="office-selector">Organisation:</InputLabel>
-      <Select
-        labelId="office-selector"
-        value={offices.find((o) => user.officeId === o.id)}
-        label="Office"
+    <>
+      <Box
+        sx={{
+          display: 'grid',
+          gridGap: `${padding.minimal} ${padding.large}`,
+          paddingTop: padding.standard,
+          paddingBottom: padding.large,
+          gridTemplateColumns: 'max-content max-content',
+        }}
       >
-        {offices.map((office) => (
-          <MenuItem value={office.id}>{office.name}</MenuItem>
-        ))}
-      </Select>
-    </FormControl>
-      </Box></>
+        <Typography>Namn</Typography>
+        <Typography sx={{ fontWeight: 400 }}>{azureUser.displayName}</Typography>
+        <Typography>Epost</Typography>
+        <Typography sx={{ fontWeight: 400 }}>{azureUser.mail}</Typography>
+      </Box>
+      <img src={`${test}`} srcSet={`${test}`} alt="" loading="lazy" />
+      <Box marginTop={0}>
+        <TextField
+          fullWidth
+          multiline
+          minRows={7}
+          required
+          name="Speaker bio"
+          label="Talarbio: (visas upp på dina kompetensbidrag, var tydlig med din roll och din kompetens!)"
+          variant="outlined"
+        />
+      </Box>
+      <Box marginTop={0} marginBottom={5}>
+        <FormControl fullWidth>
+          <InputLabel id="office-selector">Organisation:</InputLabel>
+          <Select
+            labelId="office-selector"
+            value={offices.find((o) => user.officeId === o.id)}
+            label="Office"
+          >
+            {offices.map((office) => (
+              <MenuItem value={office.id}>{office.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+    </>
   );
 };
 export default Profile;
