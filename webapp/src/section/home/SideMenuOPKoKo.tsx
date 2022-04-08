601a7f503b11d95267d99e67a1b1b8e5148ab2c5
Rebecca Hellström Karlsson <rebecca.hellstrom@gmail.com>
2022-04-08 12:07:34 +0200

    Render lectures again

--- webapp/src/section/home/SideMenuOPKoKo.tsx
+++ webapp/src/section/home/SideMenuOPKoKo.tsx
@@ -1,31 +1,23 @@
 import { Button } from '@mui/material';
 import { ReactElement } from 'react';
-import { useQuery } from 'react-query';
-import { listLectures } from '../../api/Api';
 import KoKoEvents from '../../components/KoKoEvents/KoKoEvents';
 import LatestLectures from '../../components/latestLectures/LatestLectures';
 import SideCard from '../../components/sideCard/SideCard';
 
-const SideMenuOPKoKo = (): ReactElement => {
-  const { data } = useQuery(`listMyLectures`, () => listLectures({ mine: 'true' }));
-  return (
-    <>
+const SideMenuOPKoKo = (): ReactElement => (
+  <>
+    {' '}
+    <SideCard headerText="Kommande OPKoKos">
+      <KoKoEvents />
+    </SideCard>
+    <SideCard headerText="Mina bidrag">
       {' '}
-      <SideCard headerText="Kommande OPKoKos">
-        <KoKoEvents />
-      </SideCard>
-      <SideCard
-        title={data === undefined || data.length > 0 ? 'Mina inskickade bidrag' : ''}
-        headerText="Mina bidrag"
-      >
-        {' '}
-        <LatestLectures />
-        <Button variant="contained" href="/lecture/user">
-          Hantera inskickade bidrag
-        </Button>
-      </SideCard>
-    </>
-  );
-};
+      <LatestLectures />
+      <Button variant="contained" href="/lecture/user">
+        Hantera inskickade bidrag
+      </Button>
+    </SideCard>
+  </>
+);
 
 export default SideMenuOPKoKo;
