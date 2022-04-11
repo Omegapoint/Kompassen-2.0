36d0bdf3e99726bdb9aea9451797473eb58e7f9c
Hannes Samskog <hannes.samskog@omegapoint.se>
2022-04-11 10:27:46 +0200

    Fixes merge conflicts

--- webapp/src/section/home/SideMenuOPKoKo.tsx
+++ webapp/src/section/home/SideMenuOPKoKo.tsx
@@ -1,17 +1,15 @@
 import { Button } from '@mui/material';
 import { ReactElement } from 'react';
-import KoKoEvents from '../../components/KoKoEvents/KoKoEvents';
+import { useQuery } from 'react-query';
+import { listLectures } from '../../api/Api';
 import LatestLectures from '../../components/latestLectures/LatestLectures';
 import OPKoKoEvents from '../../components/OPKoKoEvents/OPKoKoSidebar';
 import SideCard from '../../components/sideCard/SideCard';
 
-const SideMenuOPKoKo = (): ReactElement => (
-  <>
-    {' '}
-    <SideCard headerText="Kommande OPKoKos">
-      <KoKoEvents />
-    </SideCard>
-    <SideCard headerText="Mina bidrag">
+const SideMenuOPKoKo = (): ReactElement => {
+  const { data } = useQuery(`listMyLectures`, () => listLectures({ mine: 'true' }));
+  return (
+    <>
       {' '}
       <SideCard headerText="Kommande OPKoKos">
         <OPKoKoEvents />
@@ -21,11 +19,12 @@ const SideMenuOPKoKo = (): ReactElement => (
         headerText="Mina bidrag"
       >
         {' '}
-      <LatestLectures />
-      <Button variant="outlined" href="/lecture/user">
-        Hantera inskickade bidrag
-      </Button>
-    </SideCard>
-  </>
-);
+        <LatestLectures />
+        <Button variant="outlined" href="/lecture/user">
+          Hantera inskickade bidrag
+        </Button>
+      </SideCard>
+    </>
+  );
+};
 export default SideMenuOPKoKo;
