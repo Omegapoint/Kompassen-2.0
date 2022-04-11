96796f7956cd1361daf2134bffba0f672446f39f
Hannes Samskog <hannes.samskog@omegapoint.se>
2022-04-11 08:59:39 +0200

    Removes unused variable

--- webapp/src/components/OPKoKoPlanner/opkokoPlanner.tsx
+++ webapp/src/components/OPKoKoPlanner/opkokoPlanner.tsx
@@ -1,13 +1,11 @@
 import { Box } from '@mui/material';
 import { ReactElement } from 'react';
 import { useParams } from 'react-router-dom';
-import { useEvent } from '../../hooks/UseReduxState';
 import RegisteredLectures from '../../section/competencedayPlanner/RegisteredLectures';
 import useEventLecturesWS from '../../section/competencedayPlanner/UseEventLecturesWS';
 
 const OPKoKoPlanner = (): ReactElement => {
   const { id } = useParams<'id'>();
-  const event = useEvent(id!)!;
   const lectures = useEventLecturesWS(id!);
 
   return (
