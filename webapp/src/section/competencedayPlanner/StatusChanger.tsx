f7ccf0713e9fc17351f66677c4193c9b24978021
Ida Petersson <89082289+ipomega@users.noreply.github.com>
2022-04-26 00:20:29 +0200

    Fixes

--- webapp/src/section/competencedayPlanner/StatusChanger.tsx
+++ webapp/src/section/competencedayPlanner/StatusChanger.tsx
@@ -1,12 +1,17 @@
 import { ArrowDropDown } from '@mui/icons-material';
-import { ListItem, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import { MenuItem, Select, SelectChangeEvent } from '@mui/material';
 import { ReactElement, useEffect, useState } from 'react';
 import { setLectureStatus } from '../../api/Api';
-import { checkAccess, ROLE } from '../../lib/Lib';
+import { checkAccess, ROLE, useAppSelector } from '../../lib/Lib';
 import { colors } from '../../theme/Theme';
 
-const PlannerMenu = (lectureID: any): ReactElement => {
+interface StatusChangerProps {
+  lectureID: string;
+}
+
+const StatusChanger = ({ lectureID }: StatusChangerProps): ReactElement => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const statuses = useAppSelector((state) => state.statuses);
   const [status, setStatus] = useState('');
   useEffect(() => {
     setLectureStatus({ statusID: status, lectureID });
@@ -21,7 +26,6 @@ const PlannerMenu = (lectureID: any): ReactElement => {
       {checkAccess([ROLE.ADMIN, ROLE.OPKOKO_PLANNER, ROLE.OPKOKO_PROGRAM_COMMITTEE]) && (
         <>
           <Select
-            defaultValue="Ohanterad"
             onChange={handleChange}
             sx={{
               height: '40px',
@@ -31,18 +35,11 @@ const PlannerMenu = (lectureID: any): ReactElement => {
               padding: 0,
             }}
           >
-            <MenuItem value="Ohanterad" onClick={handleClose}>
-              <ListItem>Ohanterad</ListItem>
-            </MenuItem>
-            <MenuItem value="Accepterad" onClick={handleClose}>
-              <ListItem>Accepterad</ListItem>
-            </MenuItem>
-            <MenuItem value="Feedback" onClick={handleClose}>
-              <ListItem>Feedback</ListItem>
-            </MenuItem>
-            <MenuItem value="Nekad" onClick={handleClose}>
-              <ListItem>Nekad</ListItem>
-            </MenuItem>
+            {statuses.map((e) => (
+              <MenuItem key={e.id} value={e.id}>
+                {e.name}
+              </MenuItem>
+            ))}
             <ArrowDropDown htmlColor={colors.white} />
           </Select>
         </>
@@ -51,4 +48,4 @@ const PlannerMenu = (lectureID: any): ReactElement => {
   );
 };
 
-export default PlannerMenu;
+export default StatusChanger;
