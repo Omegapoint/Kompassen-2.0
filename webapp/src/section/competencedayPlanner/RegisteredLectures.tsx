0f04c07fb3389f9cc82b8abe5fef15ecbda7ea6f
Hannes Samskog <hannes.samskog@omegapoint.se>
2022-04-26 21:00:38 +0200

    In KoKo admin view puts the accepted lectures on top

--- webapp/src/section/competencedayPlanner/RegisteredLectures.tsx
+++ webapp/src/section/competencedayPlanner/RegisteredLectures.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material';
 import { ReactElement } from 'react';
+import { useAppSelector } from '../../lib/Lib';
 import { Lecture } from '../../lib/Types';
 import { padding } from '../../theme/Theme';
 import LectureCard from './LectureCard';
@@ -14,19 +15,42 @@ const RegisteredLectures = ({
   lectures,
   admin = false,
   opkoko = false,
-}: RegisteredLecturesProps): ReactElement => (
-  <Box
-    sx={{
-      display: 'grid',
-      gridTemplateColumns: opkoko ? 'max-content' : '1fr 1fr 1fr',
-      gridTemplateRows: 'max-content',
-      gridGap: padding.standard,
-    }}
-  >
-    {lectures.map((e) => (
-      <LectureCard key={e.id} lecture={e} edit admin={admin} opkoko={opkoko} />
-    ))}
-  </Box>
-);
+}: RegisteredLecturesProps): ReactElement => {
+  const statuses = useAppSelector((state) => state.statuses);
+  return (
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: opkoko ? 'max-content' : '1fr 1fr 1fr',
+        gridTemplateRows: 'max-content',
+        gridGap: padding.standard,
+      }}
+    >
+      {lectures
+        .filter((lecture) =>
+          statuses.some(
+            (status) =>
+              status.id === lecture.status?.statusID &&
+              (status.name === 'Accepterad' || status.name === 'Feedback')
+          )
+        )
+        .map((e) => (
+          <LectureCard key={e.id} lecture={e} edit admin={admin} opkoko={opkoko} />
+        ))}
+      {lectures
+        .filter(
+          (lecture) =>
+            !statuses.some(
+              (status) =>
+                status.id === lecture.status?.statusID &&
+                (status.name === 'Accepterad' || status.name === 'Feedback')
+            )
+        )
+        .map((e) => (
+          <LectureCard key={e.id} lecture={e} edit admin={admin} opkoko={opkoko} />
+        ))}
+    </Box>
+  );
+};
 
 export default RegisteredLectures;
